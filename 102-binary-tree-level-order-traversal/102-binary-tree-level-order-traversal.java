0c801dccd31417b877107241a9f097475f874a37
Brian Lau <103338659+beelauuu@users.noreply.github.com>
2022-08-26 11:52:00 -0400

    Time: 1 ms (93.23%), Space: 42.2 MB (96.16%) - LeetHub

--- 102-binary-tree-level-order-traversal/102-binary-tree-level-order-traversal.java
+++ 102-binary-tree-level-order-traversal/102-binary-tree-level-order-traversal.java
@@ -15,21 +15,34 @@
  */
 class Solution {
     public List<List<Integer>> levelOrder(TreeNode root) {
+    //Node levelOrder is just a fancy term for BFS
+        
+        //Value to return
         List<List<Integer>> list = new ArrayList<>();
         
+        //If the root is null, we can just return an empty list
         if(root == null) {
             return list;
         }
         
+        
+        //Otherwise create your queue and add your root
         Queue<TreeNode> queue = new LinkedList();
         queue.add(root);
         
+        //While it's not empty
         while(!queue.isEmpty()) {
+            //We can have a size value for each level and a list for each level
             int size = queue.size();
             List<Integer> currLevel = new ArrayList<>();
+            
+            //Iterate through all the nodes on each level removing from queue and adding it
+            //to the curr level
             for(int i = 0; i < size; i++) {
                 TreeNode curr = queue.remove();
                 currLevel.add(curr.val);
+                
+                //If it's got children, add the left and right to the queue
                 if(curr.left != null) {
                     queue.add(curr.left);
                 }
@@ -37,6 +50,8 @@ class Solution {
                     queue.add(curr.right);
                 }
             }
+            
+            //Finally add the list to the returner
             list.add(currLevel);
         }
         
