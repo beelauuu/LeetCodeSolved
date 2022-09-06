f8e46243a127379f4a6bd740823c131d49dc49c8
Brian Lau <103338659+beelauuu@users.noreply.github.com>
2022-09-05 20:57:05 -0400

    Time: 1 ms (44.50%), Space: 42.4 MB (64.68%) - LeetHub

--- 153-find-minimum-in-rotated-sorted-array/153-find-minimum-in-rotated-sorted-array.java
+++ 153-find-minimum-in-rotated-sorted-array/153-find-minimum-in-rotated-sorted-array.java
@@ -13,19 +13,24 @@ class Solution {
         int right = nums.length-1;
         
         //Basic binary search
-        while(left < right) {
+        while(left <= right) {
             int mid = (left+right)/2;
             
+            //We know that if nums[mid] is less than the element before it, that it is
+            //the splitting point of the array and that mid is the minimum element
+            if(mid > 0 && nums[mid] < nums[mid-1]) {
+                return nums[mid];
+            }
             //Else we haven't found it yet, check if the left is less than the middle.
             //We know then that the left is sorted. Check the right also to see if it is
             //the unsorted part
-            if(nums[mid] > nums[right]) {
+            else if(nums[left] <= nums[mid] && nums[mid] > nums[right]) {
                 left = mid+1;
             }
             //Otherwise, it's either perfectly sorted or the right is out of order, in both
             //cases, the right pointer will move to mid-1
             else {
-                right = mid;
+                right = mid-1;
             }
         }
         //Return the left pointer
