ac98c37440739423e0263ea9b469119e14b835f6
Brian Lau <103338659+beelauuu@users.noreply.github.com>
2022-09-10 15:00:20 -0400

    Time: 1344 ms (5.44%), Space: 49.8 MB (17.02%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.java
+++ 125-valid-palindrome/125-valid-palindrome.java
@@ -1,8 +1,11 @@
 class Solution {
     public boolean isPalindrome(String s) {
+        
+        //Java Regex In This Case, Any character except A-Z, a-z, or 0-9.
         String copy = s.replaceAll("[^A-Za-z0-9]","");
         copy = copy.toLowerCase();
         
+        //Copying it over and adding it
         char[] array = copy.toCharArray();
         List<Character> og = new ArrayList<>();
         List<Character> rev = new ArrayList<>();
@@ -14,12 +17,19 @@ class Solution {
         
         Collections.reverse(rev);
         
+        //Comparing each value
         for(int i = 0; i < og.size(); i++) {
             if(og.get(i) != rev.get(i)) {
                 return false;
             }
         }
         
+        //If asked for inplace 
+        // for(int i = 0 ; i < copy.length()/2; i++) {
+        //     if(copy.charAt(i) != copy.charAt(copy.length()-i-1)) {
+        //         return false;
+        //     }
+        // }
         
         return true;
     }
