d08bfb8d4a9ff8a160811e9172f0177f3363867e
Brian Lau <103338659+beelauuu@users.noreply.github.com>
2022-09-10 23:37:42 -0400

    Time: 34 ms (16.40%), Space: 52.9 MB (24.61%) - LeetHub

--- 703-kth-largest-element-in-a-stream/703-kth-largest-element-in-a-stream.java
+++ 703-kth-largest-element-in-a-stream/703-kth-largest-element-in-a-stream.java
@@ -6,6 +6,7 @@ class KthLargest {
     public KthLargest(int k, int[] nums) {
         this.k = k;
         
+        //Adding in the initial numbers into the stream
         for(int i = 0; i < nums.length; i++) {
         add(nums[i]);
         }
@@ -13,12 +14,15 @@ class KthLargest {
     }
     
     public int add(int val) {
+        //Add in the value to add
         pq.add(val);
         
+        //If the size is greater than the kth value to return, just remove the smallest one
         if(pq.size() > k) {
             pq.poll();
         }
         
+        //The head of the pq is going to be the kth element everytime
         return pq.peek();
     }
 }
