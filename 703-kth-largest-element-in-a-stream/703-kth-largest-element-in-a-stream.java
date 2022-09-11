27ddb58c8e5237b457804858678cd222474075f7
Brian Lau <103338659+beelauuu@users.noreply.github.com>
2022-09-10 23:24:41 -0400

    Time: 33 ms (18.67%), Space: 52.6 MB (51.16%) - LeetHub

--- 703-kth-largest-element-in-a-stream/703-kth-largest-element-in-a-stream.java
+++ 703-kth-largest-element-in-a-stream/703-kth-largest-element-in-a-stream.java
@@ -5,10 +5,11 @@ class KthLargest {
     
     public KthLargest(int k, int[] nums) {
         this.k = k;
-        for(int i = 0; i < nums.length; i++) {
-            add(nums[i]);
-        }
         
+        for(int i = 0; i < nums.length; i++) {
+        add(nums[i]);
+        }
+
     }
     
     public int add(int val) {
