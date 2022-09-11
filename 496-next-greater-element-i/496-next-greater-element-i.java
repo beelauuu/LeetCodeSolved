4dbbfbdfcc54d62ab04f9fca6b48841a794c3864
Brian Lau <103338659+beelauuu@users.noreply.github.com>
2022-09-11 18:38:04 -0400

    Time: 6 ms (53.98%), Space: 44.3 MB (45.73%) - LeetHub

--- 496-next-greater-element-i/496-next-greater-element-i.java
+++ 496-next-greater-element-i/496-next-greater-element-i.java
@@ -3,10 +3,18 @@ class Solution {
         HashMap<Integer, Integer> next_greatest = new HashMap();
         Stack<Integer> stack = new Stack();
         
+        //Loop through the larger int array
         for(Integer num: nums2) {
+            //If there is a next_greatest number, pop the current stack and then
+            //map it in the hashmap
             while(!stack.isEmpty() && stack.peek() < num) {
                 next_greatest.put(stack.pop(), num);
             }
+            //This is a while-loop because assuming the case of [6,5,4,3,2,1,7]
+            //Stack is going to contain each of the first 6 numbers and then when 7 is
+            //reached all the numbers will be popped()
+            
+            //Push every number onto the stack
             stack.push(num);
         }
         
