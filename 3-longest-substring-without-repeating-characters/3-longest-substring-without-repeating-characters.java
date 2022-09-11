3b5bceb078369f41a2bf030cb41026f8fa1838bd
Brian Lau <103338659+beelauuu@users.noreply.github.com>
2022-09-11 15:11:54 -0400

    Time: 19 ms (29.57%), Space: 45.9 MB (40.24%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.java
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.java
@@ -5,12 +5,15 @@ class Solution {
         int max = 0;
         HashSet<Character> set = new HashSet();
         
+        //Two pointers (sliding window)
         while(j < s.length()) {
+            //If the set doesn't contain the character, increment and add j.
             if(!set.contains(s.charAt(j))) {
                 set.add(s.charAt(j++));
                 max = Math.max(set.size(), max);
             }
             else {
+                //Otherwise slide up your beginning for the duplicate.
                 set.remove(s.charAt(i++));
             }
         }
