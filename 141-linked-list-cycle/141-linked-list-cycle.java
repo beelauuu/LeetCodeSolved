23aa6020ffeab5609b6e3c857b7e45b311a89eff
Brian Lau <103338659+beelauuu@users.noreply.github.com>
2022-09-12 21:59:09 -0400

    Time: 1 ms (48.98%), Space: 45.8 MB (51.75%) - LeetHub

--- 141-linked-list-cycle/141-linked-list-cycle.java
+++ 141-linked-list-cycle/141-linked-list-cycle.java
@@ -11,19 +11,17 @@
  */
 public class Solution {
     public boolean hasCycle(ListNode head) {
-        //Check for null at first
-        if(head==null)
+        if(head == null) {
             return false;
+        }
         
-        //Algorithm called tortoise & hare
-        ListNode slow=head;
-        ListNode fast=head;
+        ListNode slow = head;
+        ListNode fast = head;
         
-        //If we touch null or the next value being null, we know there's no cycle
-        while(fast!=null && fast.next!=null) {
-            slow=slow.next;
-            fast=fast.next.next;
-            //Eventually slow and fast will meet if there is
+        while(fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            
             if(slow == fast) {
                 return true;
             }
