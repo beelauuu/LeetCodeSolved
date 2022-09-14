54000ca3780b8433def4e6b08b8ae596194c1ee9
Brian Lau <103338659+beelauuu@users.noreply.github.com>
2022-09-13 22:24:31 -0400

    Time: 115 ms (30.88%), Space: 99.5 MB (93.24%) - LeetHub

--- 435-non-overlapping-intervals/435-non-overlapping-intervals.java
+++ 435-non-overlapping-intervals/435-non-overlapping-intervals.java
@@ -17,13 +17,20 @@ class Solution {
         int[] curr = intervals[0];
         
         for(int i = 1; i < intervals.length; i++) {
-            if(curr[1] > intervals[i][0]) {
+            //If the start of the next interval is less than the end of the
+            //current one
+            if(intervals[i][0] < curr[1]) {
+                //We're going to have to remove an interval
                 intervalsRemoved++;
+                
+                //If the current interval spans a longer distance than the shorter one,
+                //we'd have to remove one
                 if(curr[1] > intervals[i][1]) {
                     curr = intervals[i];
                 }
             }
             else {
+                //Otherwise, if not overlapping move on
                 curr = intervals[i];
             }
         }
