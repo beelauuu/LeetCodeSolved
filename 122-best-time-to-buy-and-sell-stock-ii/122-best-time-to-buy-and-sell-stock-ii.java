8d2e71e3d9a2e2d7187da044752572cfaebe701c
Brian Lau <103338659+beelauuu@users.noreply.github.com>
2022-10-03 13:12:03 -0400

    Time: 3 ms (30.13%), Space: 43.8 MB (78.95%) - LeetHub

--- 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.java
+++ 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.java
@@ -1,23 +1,31 @@
 class Solution {
     public int maxProfit(int[] prices) {
+        
+        //Checking edge cases
         if (prices == null || prices.length <= 1) {
             return 0;
         } 
         
+        //Setting up toReturn and the minimum stock price
         int toReturn = 0;
         int min = prices[0];
         
+        //Looping through the prices array
         for (int i = 1; i < prices.length; i++)
         {
             int curr = prices[i];
+            
+            //If the current stock price is greater than your minimum, sell for a profit
             if(curr > min) {
                 toReturn += curr-min;
                 min = curr; 
             }
+            //Otherwise, if curr is < min then set your new minimum
             else {
                 min = curr;
             }
         }
+        
         return toReturn;
     }
 }
