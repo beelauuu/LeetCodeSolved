d905d6162a7aa1115d19b2a6037e8184850e78a8
Brian Lau <103338659+beelauuu@users.noreply.github.com>
2022-10-02 22:26:36 -0400

    Time: 1 ms (52.82%), Space: 42.9 MB (22.42%) - LeetHub

--- 153-find-minimum-in-rotated-sorted-array/153-find-minimum-in-rotated-sorted-array.java
+++ 153-find-minimum-in-rotated-sorted-array/153-find-minimum-in-rotated-sorted-array.java
@@ -12,12 +12,14 @@ class Solution {
         int left = 0;
         int right = nums.length-1;
         
+        //Modified binary search
         while(left < right) {
-            int mid = (left+right)/2;
-            
+            int mid = (left+right)/2;  
+            //If the middle is greater than the right-most element, we know that the right is the smaller, unsorted side. We can do mid+1 because we know that the mid is not the smallest element
             if(nums[mid] > nums[right]) {
                 left = mid+1;
             }
+            //Else we know the smallest element is going to be on the left side
             else {
                 right = mid;
             }
