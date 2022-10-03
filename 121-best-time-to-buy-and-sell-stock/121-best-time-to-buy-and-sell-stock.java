b2751a0ceb8415e01b96bee124f5be3e3fe6ea91
Brian Lau <103338659+beelauuu@users.noreply.github.com>
2022-10-03 00:50:22 -0400

    Time: 2 ms (93.76%), Space: 59.2 MB (87.59%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.java
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.java
@@ -1,21 +1,34 @@
 class Solution {
     public int maxProfit(int[] prices) {
-    int minimum = Integer.MAX_VALUE;
-    int toReturn = 0;
-    
-    //Loop through the prices
-    for(int i = 0; i < prices.length; i++) {
-        //If there is a smaller value set it equal
-        if(prices[i] < minimum) {
-            minimum = prices[i];
-        }
-        //Otherwise check to see if the current profit is greater than prices[i]-minimum
-        else {
-            toReturn = Math.max(toReturn, prices[i]-minimum);
-        }
-    }
         
-    return toReturn;
+       if(prices.length <= 1 || prices == null) {
+           return 0;
+       }
+       int minimum = prices[0];
+       int toReturn = 0;
+        
+       for(int i = 1; i < prices.length; i++) {
+           minimum = Math.min(minimum, prices[i]);
+           toReturn = Math.max(toReturn, prices[i]-minimum);
+       }
+       return toReturn;
+//     int minimum = Integer.MAX_VALUE;
+//     int toReturn = 0;
+    
+//     //Loop through the prices
+//     for(int i = 0; i < prices.length; i++) {
+//         //If there is a smaller value set it equal
+//         if(prices[i] < minimum) {
+//             minimum = prices[i];
+//         }
+//         //Otherwise check to see if the current profit is greater than prices[i]-minimum
+//         else {
+//             toReturn = Math.max(toReturn, prices[i]-minimum);
+//         }
+//     }
+        
+//     return toReturn;
+
     
         
 }
